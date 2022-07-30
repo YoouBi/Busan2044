62a8d590ff2cfd76cc65da6da1f260381db641d1
YoouBi <104603807+YoouBi@users.noreply.github.com>
2022-07-30 23:34:22 +0900

    Add files via upload

---
+++ Pathget.java
@@ -0,0 +1,47 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Pathget {
+	public String[] getPathAll(String id, int pathNum) {
+		String[] savepath;
+		try (Connection conn = BusanUtil.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM Busan.savehere");
+				ResultSet rs = pstmt.executeQuery();) {
+			
+			String yourSave1 = "";
+			String yourSave2 = "";
+			while (rs.next()) {
+				if (id.equals(rs.getString("userId"))) {
+					yourSave1 = rs.getString("storyPath1");
+					yourSave2 = rs.getString("storyPath2");
+				}
+			}
+			
+			yourSave1 = yourSave1.replace(" ", "").replace("[", "").replace("]", "");
+			yourSave2 = yourSave2.replace(" ", "").replace("[", "").replace("]", "");
+			String[] saveArr1 = yourSave1.split(",");
+			String[] saveArr2 = yourSave2.split(",");
+			
+			if(pathNum == 1) {
+				savepath = saveArr1;
+			} else if(pathNum == 2) {
+				savepath = saveArr2;
+			}
+		}
+		
+		return savepath;
+	}
+	
+	public String[] getPath1(String id) {
+		return getPathAll(id, 1);
+	}
+	
+	public String[] getPath2(String id) {		
+		return getPathAll(id, 2);
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
